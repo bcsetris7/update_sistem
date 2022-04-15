faa5bd5a3536b8d645244f4c4c9c0bd4d398fe12
Bcset ris7 <102164567+bcsetris7@users.noreply.github.com>
2022-04-15 14:21:22 +0700

    Create up.sh

---
+++ OSINT/up.sh
@@ -0,0 +1,23 @@
+clear
+echo -e "$Yellow                            processing please wait..>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait...>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait.....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait......>"
+sleep 2.0
+clear
+echo " "
+cd $HOME
+rm -rf OSINT
+
+#masukan per mau
+git clone https://github.com/bcsetris7/OSINT
+ 
